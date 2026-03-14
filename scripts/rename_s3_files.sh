4c6721518d63b59ce188e2159dbf906d0876724d
aarushkadira-dot <aarush.kadira@gmail.com>
2026-03-14 17:46:20 -0400

    add s3 rename script to label files by species

---
+++ scripts/rename_s3_files.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+BUCKET="ssmathhacks"
+PREFIX="ssm-hackathon"
+
+aws s3 mv s3://$BUCKET/$PREFIX/ML110880.mp3  s3://$BUCKET/$PREFIX/sperm_whale_ML110880.mp3
+aws s3 mv s3://$BUCKET/$PREFIX/ML111991.mp3  s3://$BUCKET/$PREFIX/pilot_whale_ML111991.mp3
+aws s3 mv s3://$BUCKET/$PREFIX/ML112410.mp3  s3://$BUCKET/$PREFIX/false_killer_whale_ML112410.mp3
+aws s3 mv s3://$BUCKET/$PREFIX/ML112936.mp3  s3://$BUCKET/$PREFIX/sperm_whale_ML112936.mp3
+aws s3 mv s3://$BUCKET/$PREFIX/ML116253.mp3  s3://$BUCKET/$PREFIX/beaked_whale_ML116253.mp3
+aws s3 mv s3://$BUCKET/$PREFIX/ML116261.mp3  s3://$BUCKET/$PREFIX/beaked_whale_ML116261.mp3
+aws s3 mv s3://$BUCKET/$PREFIX/ML116550.mp3  s3://$BUCKET/$PREFIX/beaked_whale_ML116550.mp3
+aws s3 mv s3://$BUCKET/$PREFIX/ML117702.mp3  s3://$BUCKET/$PREFIX/sperm_whale_ML117702.mp3
+aws s3 mv s3://$BUCKET/$PREFIX/ML117723.mp3  s3://$BUCKET/$PREFIX/pilot_whale_ML117723.mp3
+aws s3 mv s3://$BUCKET/$PREFIX/ML117736.mp3  s3://$BUCKET/$PREFIX/pilot_whale_ML117736.mp3
+aws s3 mv s3://$BUCKET/$PREFIX/ML120530.mp3  s3://$BUCKET/$PREFIX/false_killer_whale_ML120530.mp3
+aws s3 mv s3://$BUCKET/$PREFIX/ML120685.mp3  s3://$BUCKET/$PREFIX/false_killer_whale_ML120685.mp3
+aws s3 mv s3://$BUCKET/$PREFIX/ML128219.mp3  s3://$BUCKET/$PREFIX/orca_ML128219.mp3
+aws s3 mv s3://$BUCKET/$PREFIX/ML128271.mp3  s3://$BUCKET/$PREFIX/sperm_whale_ML128271.mp3
+aws s3 mv s3://$BUCKET/$PREFIX/ML128272.mp3  s3://$BUCKET/$PREFIX/sperm_whale_ML128272.mp3
+aws s3 mv s3://$BUCKET/$PREFIX/ML134008.mp3  s3://$BUCKET/$PREFIX/orca_ML134008.mp3
+aws s3 mv s3://$BUCKET/$PREFIX/ML134015.mp3  s3://$BUCKET/$PREFIX/fin_whale_ML134015.mp3
+aws s3 mv s3://$BUCKET/$PREFIX/ML134010.mp3  s3://$BUCKET/$PREFIX/sperm_whale_ML134010.mp3
+aws s3 mv s3://$BUCKET/$PREFIX/ML112299.mp3  s3://$BUCKET/$PREFIX/sperm_whale_ML112299.mp3
+aws s3 mv s3://$BUCKET/$PREFIX/ML128263.mp3  s3://$BUCKET/$PREFIX/blue_whale_ML128263.mp3
